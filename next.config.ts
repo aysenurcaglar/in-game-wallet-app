c010c90abfbdc14d8b586ab63a4f4155a99440f9
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-11-27 22:22:38 +0300

    removed next config content as it was causing issues with google auth

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.braintreegateway.com",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
